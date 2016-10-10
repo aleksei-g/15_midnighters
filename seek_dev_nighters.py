cc536065ac94a3d34649ac1d2669fbc41adb2a26
aleksei-g <lexa_prorock@mail.ru>
2016-10-10 15:58:33 +0300

    задача решена

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,52 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import requests
+from pytz import timezone
+from datetime import datetime, time
 
-def get_midnighters():
-    pass
+
+URL_DEVMAN_API = 'http://devman.org/api/challenges/solution_attempts/'
+START_TIME_OF_NIGHT = time(0, 0, 0)
+END_TIME_OF_NIGHT = time(5, 59, 59)
+
+
+def load_attempts(url):
+    pages = 1
+    page = 1
+    while page <= pages:
+        response = requests.get(url, params={'page': page})
+        if response.status_code == requests.codes.ok:
+            if page == 1:
+                pages = int(response.json()['number_of_pages'])
+            page += 1
+            records = response.json()['records']
+            for record in records:
+                yield {
+                       'username': record['username'],
+                       'timestamp': record['timestamp'],
+                       'timezone': record['timezone'],
+                      }
+
+
+def get_midnighters(records):
+    midnighters = set()
+    for record in records:
+        if record['timestamp'] and record['timezone']:
+            time_record = datetime.fromtimestamp(
+                                                 record['timestamp'],
+                                                 timezone(record['timezone'])
+                                                 ).time()
+            if START_TIME_OF_NIGHT <= time_record <= END_TIME_OF_NIGHT:
+                midnighters.add(record['username'])
+    return midnighters
+
+
+def output_midnighters(midnighters):
+    if midnighters:
+        print('"Совы" на devman.org:')
+        for midnighter in midnighters:
+            print(midnighter)
+
 
 if __name__ == '__main__':
-  pass
+    records = load_attempts(URL_DEVMAN_API)
+    midnighters = get_midnighters(records)
+    output_midnighters(midnighters)
